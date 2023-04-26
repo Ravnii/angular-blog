6038e9409bf268d63d319182f0df2ba4cd0b2891
Jesper Ravn <jspr.ravn@gmail.com>
2023-04-26 08:18:51 +0200

    Clean repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -13,26 +12,36 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { AppRoutingModule } from "./app-routing.module";
-
-import { NavigationComponent } from './navigation/navigation.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { TableViewComponent } from './table-view/table-view.component';
-import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatTableModule } from '@angular/material/table';
+import { MatTreeModule } from '@angular/material/tree';
 import { MatSortModule } from '@angular/material/sort';
-import { FormComponent } from './form/form.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { TableViewComponent } from './table-view/table-view.component';
+import { FormComponent } from './form/form.component';
 import { DetailsComponent } from './details/details.component';
-import { MatTreeModule } from '@angular/material/tree';
+
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, DashboardComponent, ToDoListComponent, TableViewComponent, FormComponent, DetailsComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    ToDoListComponent,
+    TableViewComponent,
+    FormComponent,
+    DetailsComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
